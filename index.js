9eac6380e8466daec0ab306f3a86c83230893518
SharminMily <sharminmily039@gmail.com>
2023-11-07 12:27:44 +0600

    category country data load

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const categoryCollection = client.db('travelBlog').collection('category');
+
+        app.get('/category', async(req, res) => {
+            const cursor = categoryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
